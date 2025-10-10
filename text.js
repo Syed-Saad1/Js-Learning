9decca6d6031bba334732b4fcf4d8945070ed01a
syed-saad-1 <sfurqanarif7528@gmail.com>
2025-10-09 17:45:44 -0700

    First

---
+++ text.js
@@ -0,0 +1,49 @@
+// console.log("Mr:Saad");
+
+// let Car_Name = "Civic Rs ";
+// let Car_title = "This Car is Honda Company This is Very Bestest Car!";
+// let Car_Price = "10,100,000 ";
+// let Car_Color = "Black";
+// let Car_Model = 2025;
+//  Car_title = "North America, the top trims for the 2025 model year are the Sport Touring Hybrid, Civic Si, and Type R.";
+// let Array = [Car_Name, Car_title, Car_Price, Car_Color, Car_Model];
+// let Array1 = [Car_Name, Car_title, Car_Price, Car_Color, Car_Model];
+
+// console.table(Array);
+
+// let string = "Saad";
+// let number = 16;
+// let bolean =true;
+// let null1 = null;
+// let undefind;
+
+// let Array2 =[string,number,bolean,null1,undefind];
+// console.log( Array2);
+
+// let score = true;
+// console.log(typeof (score));
+
+// let value = Number(score);
+// console.log(typeof value);
+// console.log( value);
+
+// let loggedin = "";
+// let boliean = Boolean(loggedin);
+
+// console.log(boliean)
+
+let number = 384;
+let strnum = String(number);
+console.log(strnum);
+console.log(typeof strnum);
+
+let num = 2983;
+let negative = -num;
+console.log(negative);
+
+let name1 = "Syed Saad ";
+let name2 = "Raza";
+
+let final = name1 + name2;
+
+console.log(final);
